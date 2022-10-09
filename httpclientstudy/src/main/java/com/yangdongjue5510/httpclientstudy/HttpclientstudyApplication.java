29817af11cbd2a823f315db68e27c0fc7aa72b58
yangdongjue5510 <djwhy5510@gmail.com>
2022-10-09 14:20:53 +0900

    chore: 프로젝트 생성

---
+++ httpclientstudy/src/main/java/com/yangdongjue5510/httpclientstudy/HttpclientstudyApplication.java
@@ -0,0 +1,13 @@
+package com.yangdongjue5510.httpclientstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpclientstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpclientstudyApplication.class, args);
+    }
+
+}
---
+++ httpclientstudy/src/test/java/com/yangdongjue5510/httpclientstudy/HttpclientstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangdongjue5510.httpclientstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpclientstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
